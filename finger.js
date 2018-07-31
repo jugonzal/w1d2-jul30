1a5379eb84153231908718309a45cd4b1c4a06e8
Juan Gonzalez <jugonzal@gmail.com>
2018-07-31 10:12:45 -0400

    All good... tested and all

--- finger.js
+++ finger.js
@@ -25,7 +25,21 @@ var rightRoamingFingers = 10 - right;
 console.log('Left Hand: joined - roaming ', leftJoinedFingers, leftRoamingFingers);
 console.log('Right Hand: joined - roaming ', rightJoinedFingers, rightRoamingFingers);
   
-// 3. Join all the fingers that reprent the 2 numbers being multiplied
+// 3. Join all the fingers that represent the 2 numbers being multiplied
+var joinedFingers = leftJoinedFingers +rightJoinedFingers;
+
 // 4. Then count how many fingers there are joined....    X 10
+joinedFingers *= 10;
+// joinedFingers = joinedFingers * 10;
+
+console.log('Joined: ', joinedFingers);
 // 5. Lastly count roaming fingers from left hand and multiply by roaming fingers in right hand.
+
+var roamingFingers = leftRoamingFingers * rightRoamingFingers;
+console.log('Roaming: ', roamingFingers);
+
 // 6. Final step: add step #4 with step #5
+
+console.log('Final: ', joinedFingers + roamingFingers);
+console.log('Proof: ', left * right)
+
